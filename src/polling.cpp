b2541a7548c602613c1341bbbcac2209f8472558
rightfold <rightfold@gmail.com>
2017-06-25 16:40:29 +0200

    Assert that float memcpy is safe

--- src/polling.cpp
+++ src/polling.cpp
@@ -5,6 +5,13 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <limits>
+
+static_assert(
+  std::numeric_limits<float>::is_iec559,
+  "Currently only platforms with IEEE 754 floating-point numbers are "
+  "supported. Your platform is not one of these platforms."
+);
 
 boost::optional<sb::message> sb::parse(std::uint8_t const* data, std::size_t size) {
   if (size % 4 != 0) {
